1a4be6ab33a00ea00bf17ef88e7ecf8351aab2c9
Mickey <micksheridan.24@gmail.com>
2019-03-13 08:52:09 -0400

    submit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,50 @@
 
-require_relative '../../config/environment'
+require_relative "../../config/environment"
 
 class ApplicationController < Sinatra::Base
-
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :public_folder, "public"
+    set :views, "app/views"
   end
 
-  get '/' do
+  get "/" do
+    redirect "/articles"
+  end
+
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+
+  get "/articles/new" do
+    erb :new
+  end
+
+  get "/articles/:id" do
+    @article = Article.all.find(params[:id])
+    erb :show
+  end
+
+  get "/articles/:id/edit" do
+    @article = Article.all.find(params[:id])
+    erb :edit
+  end
+
+  patch "/articles/:id" do
+    @article = Article.all.find_by_id(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+    redirect "/articles/#{params[:id]}"
+  end
+
+  post "/articles" do
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect "/articles/#{@article.id}"
+  end
+
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect "/articles"
   end
 end
